6c49d292e07648d78a58a7a4c9a4b61d6be1ce66
sean <sean.c.spinney@gmail.com>
2024-05-01 09:05:38 -0400

    Improve visualizations

--- test_pcoeffs2.py
+++ test_pcoeffs2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from compute_pcoeffs import *
 
 
-def generate_dummy_data(num_communities, max_community_node, min_community_node, interconnectivity):
+def generate_dummy_data(num_communities, max_community_node, min_community_node, interconnectivity=None):
     # Generate community sizes
     community_sizes = np.random.randint(min_community_node, max_community_node + 1, size=num_communities)
     
@@ -34,10 +34,19 @@ def generate_dummy_data(num_communities, max_community_node, min_community_node,
             W_within_communities[node, selected_node] = 1
     
     # Drop an edge within the first community
-    first_community_nodes = np.where(Ci == 2)[0]
-    node1, node2 = np.random.choice(first_community_nodes, size=2, replace=False)
-    W_within_communities[node1, node2] = 0
-    W_within_communities[node2, node1] = 0    
+    if interconnectivity is None:
+        first_community_nodes = np.where(Ci == 2)[0]
+        node1, node2 = np.random.choice(first_community_nodes, size=2, replace=False)
+        W_within_communities[node1, node2] = 0
+        W_within_communities[node2, node1] = 0    
+    else:
+        # use bernoulli prob to drop edges within communities
+        # except for the selected node
+        for i in range(len(Ci)):
+            for j in range(i + 1, len(Ci)):
+                if np.random.rand() < interconnectivity and i != selected_node and j != selected_node:
+                    W_within_communities[i, j] = 0
+                    W_within_communities[j, i] = 0
 
     # Combine within community connections
     W = W_within_communities
@@ -45,10 +54,16 @@ def generate_dummy_data(num_communities, max_community_node, min_community_node,
     return W, Ci, selected_node
 
 
-def plot_network_with_participation(G, PC_norm, selected_node):
+def plot_network_with_participation(G, PC_norm, selected_node, community_colors=None):
     # Draw the network
     pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, font_size=8)
+    
+    if community_colors is not None:
+        node_colors = [community_colors[node] for node in G.nodes()]
+        nx.draw(G, pos, with_labels=False, node_color=node_colors, node_size=500, font_size=8)
+    else:
+        nx.draw(G, pos, with_labels=False, node_color='lightblue', node_size=500, font_size=8)
+    
     plt.title('Brain Network with Participation Coefficients')
     
     # Add participation coefficient as node label for the selected node
@@ -57,12 +72,11 @@ def plot_network_with_participation(G, PC_norm, selected_node):
     
     plt.show()
 
-# Case 1: Non-participation scenario
-# Generate dummy data
+# Case 1: Figure 1 A of https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7286311/#def6
 num_communities = 3
 max_community_node = 3
 min_community_node = 3
-interconnectivity = 0.1  # Probability of being connected to a community
+interconnectivity = None # Probability of being connected to a community
 W, Ci, selected_node = generate_dummy_data(num_communities, max_community_node, min_community_node, interconnectivity)
 
 PC = participation_coef(W, Ci)
@@ -74,14 +88,16 @@ print("Participation Coefficients:", PC_norm_case1)
 # Create NetworkX graph
 G_case1 = nx.from_numpy_array(W)
 
-# Plot network with participation coefficients
-plot_network_with_participation(G_case1, PC_norm_case1,selected_node)
+# Assign colors to communities
+community_colors = {node: Ci[node] for node in G_case1.nodes()}
 
-# Case 2: Full participation scenario
-num_communities = 3
+# Plot network with participation coefficients
+plot_network_with_participation(G_case1, PC_norm_case1,selected_node,community_colors)
+
+# Case 2: Figure 1 B of https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7286311/#def6num_communities = 3
 max_community_node = 6
 min_community_node = 3
-interconnectivity = 0.1  # Probability of being connected to a community
+interconnectivity = None  # Probability of being connected to a community
 W, Ci, selected_node = generate_dummy_data(num_communities, max_community_node, min_community_node, interconnectivity)
 
 PC = participation_coef(W, Ci)
@@ -93,20 +109,33 @@ print("Participation Coefficients:", PC_norm_case1)
 # Create NetworkX graph
 G_case2 = nx.from_numpy_array(W)
 
+# Assign colors to communities
+community_colors = {node: Ci[node] for node in G_case2.nodes()}
+
 # Plot network with participation coefficients
-plot_network_with_participation(G_case2, PC_norm_case2,selected_node)
+plot_network_with_participation(G_case2, PC_norm_case2,selected_node,community_colors)
 
-# Case 3: Intermediate participation scenario
-num_nodes = 20
-num_communities = 5
-W_case3, Ci_case3 = generate_dummy_data(num_nodes, num_communities)
+# Case 3: 
+num_communities = 3
+max_community_node = 10
+min_community_node = 3
+interconnectivity = 0.1  # Probability of being connected to a community
+W, Ci, selected_node = generate_dummy_data(num_communities, max_community_node, min_community_node, interconnectivity)
 
-PC_norm_case3, _, _, _ = participation_coef_norm(W_case3, Ci_case3, n_iter=10)
-print("\nCase 3 - Intermediate participation scenario:")
+PC = participation_coef(W, Ci)
+
+PC_norm_case3,PC_residual, _, Ko = participation_coef_norm(W, Ci, n_iter=10)
+print("Case 3")
 print("Participation Coefficients:", PC_norm_case3)
 
 # Create NetworkX graph
-G_case3 = nx.from_numpy_array(W_case3)
+G_case3 = nx.from_numpy_array(W)
+
+# Assign colors to communities
+community_colors = {node: Ci[node] for node in G_case3.nodes()}
+
 
 # Plot network with participation coefficients
-plot_network_with_participation(G_case3, PC_norm_case3)
+plot_network_with_participation(G_case3, PC_norm_case3,selected_node,community_colors)
+
+print("done")
